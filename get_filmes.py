ea58e04210c1996992db36bc8816be6faca1db7c
lucas <lucas.pr1@hotmail.com>
2023-04-26 17:38:19 -0300

    translated to english fixed names/years/ratings

--- get_filmes.py
+++ get_filmes.py
@@ -5,35 +5,37 @@ URL = "https://www.imdb.com/chart/moviemeter/"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
-infofilmes = soup.find('tbody', class_='lister-list').findAll('tr')
+infomovies = soup.find('tbody', class_='lister-list').findAll('tr')
+names = []
+years = []
+ratings = []
+posters = []
+links = []
+for movie in infomovies:
+    try:
+        nome = [movie.find('td', class_='titleColumn').a.text]
+        names.extend(nome)
+    except AttributeError:
+        names.extend("No name info")
 
+    try:
+        ano = [movie.find('td', class_='titleColumn').span.text]
+        years.extend(ano)
+    except AttributeError:
+        years.extend("No year info")
+    try:
+        rating = [movie.find('td', class_='ratingColumn imdbRating').strong.text]
+        ratings.extend(rating)
+    except AttributeError:
+        ratings.extend("-")
+    poster = movie.find('td', class_='posterColumn').img
+    posters.extend(poster)
 
+    link = movie.find('td', class_='titleColumn').a
+    links.extend(link)
 
-
-for filme in infofilmes:
-        try:
-            nome = [filme.find('td', class_='titleColumn').a.text]
-            print(nome)
-        except AttributeError:
-            print("sem nome")
-
-        try:
-            ano = [filme.find('td', class_='titleColumn').span.text]
-            print(ano)
-        except AttributeError:
-            print("sem ano")
-        try:
-            rating = [filme.find('td', class_='ratingColumn imdbRating').strong.text]
-            print(rating)
-        except AttributeError:
-            print("-")
-
-        poster = (filme.find('td', class_='posterColumn').img)
-        print('poster:', poster["src"])
-
-        link = (filme.find('td', class_='titleColumn').a)
-        print('link:', link["href"])
-        print('''
-        ''')
-
-
+# print(names)
+# print(years)
+# print(ratings)
+print(posters)
+print(links)
