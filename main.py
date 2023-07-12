08d87df6935aa8e21d88770b8897b0a337d64a65
Lucas Procópio <101903078+x0rgus@users.noreply.github.com>
2023-07-12 20:51:48 -0300

    Update main.py
    added operation status checker

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+
 user_agents_list = [
     'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
 ]
 
-
 HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
 URL = "https://www.imdb.com/chart/moviemeter/"
+page = requests.get(URL, headers=HEADERS)
 
-page = requests.get(URL, headers={'User-Agent': random.choice(user_agents_list)})
-soup = BeautifulSoup(page.content, "html.parser")
-print(soup)
+OPERATION_STATUS = False
+page = requests.get(URL, headers=HEADERS)
+
+match page.status_code:
+    case 200:
+        OPERATION_STATUS = True
+    case 403:
+        page = requests.get(URL, headers={'User-Agent': random.choice(user_agents_list)})
+        if page.status_code == 200:
+            OPERATION_STATUS = True
+        else:
+            print("failed to get page")
+    case _:
+        print("Failed to get page")
+
+
+if OPERATION_STATUS == True:
+    soup = BeautifulSoup(page.content, "html.parser")
+    page_content = soup.find('tbody', class_='lister-list').findAll('tr')
+    names = []
+    for movie in page_content:
+        name = movie.find('td', class_='titleColumn').a.text
+        names.append(name)
+    print(names)
